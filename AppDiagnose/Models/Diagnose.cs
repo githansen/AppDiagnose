30f17eac7521848907dbbb5520959c58fb543d2c
TheaEH <70640206+TheaEH@users.noreply.github.com>
2022-09-23 20:29:16 +0200

    Added som basic classes

---
+++ AppDiagnose/Models/Diagnose.cs
@@ -0,0 +1,8 @@
+﻿namespace AppDiagnose.Models
+{
+    public class Diagnose
+    {
+        public string navn { get; set; }
+        public string info { get; set; }
+    }
+}
---
+++ AppDiagnose/Models/Symptom.cs
@@ -0,0 +1,8 @@
+﻿namespace AppDiagnose.Models
+{
+    public class Symptom
+    {
+        public string navn { get; set; }  
+        public string katgeori { get; set; }
+    }
+}
