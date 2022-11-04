f76a1067665f91ab0ad5082a3c1f3fdfc626bae1
githansen <yohanhansen97@gmail.com>
2022-11-04 16:06:15 +0100

    Sender nå brukeren tilbake til frontend ved innloggingssjekk

--- AppDiagnose/DAL/IRepository.cs
+++ AppDiagnose/DAL/IRepository.cs
@@ -1,4 +1,6 @@
-﻿using MinDiagnose.Models;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MinDiagnose.Models;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -16,7 +18,7 @@ namespace MinDiagnose.DAL
         Task<bool> slettSymptom(int Id);
         Task<bool> CreateSymptom(string navn, int kategoriId);
         Task<bool> loggDBEndring(string beskrivelse);
-        Task<bool> logginn(Bruker bruker);
-
+        Task<Brukere> logginn(Bruker bruker);
+        Task<Brukere> ErLoggetInn(HttpContext httpContext);
     }
 }
