540966e6a9c2028a9738cdbe46b5b4c9875a6272
Sefa <info@diestus.com>
2022-11-29 18:50:01 +0100

    liten fix

--- AppDiagnose/ClientApp/src/components/konto/LoggInn.js
+++ AppDiagnose/ClientApp/src/components/konto/LoggInn.js
@@ -32,7 +32,11 @@ export const LoggInn = () => {
         }
         //Sjekker i controller om det er en bruker
         $.get("/diagnose/logginn", bruker, function (data) {
-            if (data === true) {//Om det er riktig brukernavn og passord 
+            if (data === true) {//Om det er riktig brukernavn og passord
+                setVisible(false);
+                setColor('success');
+                setText('Vellykket! Du blir nå sendt til dashboardet...');
+                setVisible(true);
                 window.location.href = "/dashboard" //Send bruker til dashboard
             } else { //Om det er feil brukernavn og passord
                 setVisible(false);
